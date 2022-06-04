0adac287486f30bcc73631c4a2c3e030b9a71e48
Koonayoung518 <koony000415@gmail.com>
2022-06-04 19:38:49 +0900

    first commit

---
+++ src/main/java/com/udoollehadminbackend/UdoollehAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.udoollehadminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdoollehAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdoollehAdminBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udoollehadminbackend/UdoollehAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udoollehadminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdoollehAdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
